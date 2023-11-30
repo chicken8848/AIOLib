eb818e961767a47a595fecddebcc1a3f6b596ad5
chicken8848 <mech_4e@hotmail.com>
2023-11-30 17:42:01 +0800

    some algo

---
+++ algorithms/insertion_sort.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+#include <stdio.h>
+
+void insertion_sort(int s[], int n) {
+  int i, j;
+
+  for (i = 1; i < n; i++) {
+    j = i;
+    while ((j > 0) && (s[j] < s[j - 1])) {
+      std::swap(s[j], s[j - 1]);
+      j = j - 1;
+    };
+  };
+}
