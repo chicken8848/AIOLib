7ada5f2ffd7ef88978ce85e94ebfad61ca016a9e
chicken8848 <mech_4e@hotmail.com>
2023-12-01 17:45:06 +0800

    added notes

---
+++ professional_cpp/namespace.cpp
@@ -0,0 +1,9 @@
+#include "namespaces.h"
+#include <iostream>
+
+namespace mycode {
+void foo() {
+  std::cout << "foo() called in mthe mycode namespace" << std::endl;
+}
+} // namespace mycode
+"
---
+++ professional_cpp/namespaces.h
@@ -0,0 +1,6 @@
+namespace mycode {
+void foo();
+}
+
+// never put using directive or using declaration in a header file,
+// otherwise you force it on everyone that is including your header
