c00fcdf56d95970d326cfd1be1a1425b46d6e127
chicken8848 <mech_4e@hotmail.com>
2023-12-04 16:23:24 +0800

    updated notes

---
+++ professional_cpp/std_array.cpp
@@ -0,0 +1,9 @@
+#include <array>
+#include <iostream>
+using namespace std;
+int main() {
+  array<int, 3> arr = {9, 8, 7};
+  cout << "Array size = " << arr.size() << endl;
+  cout << "Element 2 = " << arr[1] << endl;
+  return 0;
+}
